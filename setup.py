5ca9cdd90374b759cfb02d8777870d77650772e4
Volodymyr Savchenko <savchenk@ccage007.in2p3.fr>
2016-09-19 17:57:54 +0200

    PFILES variable

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from distutils.core import setup
 setup(
         name='pilton',
         version='1.0',
-        packages=['pilton',],
+        package_data     = {
+            "": [
+                "*.txt",
+                "*.md",
+                "*.rst",
+                "*.py"
+                ]
+            },
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
         )
