ba45986a48855a39a9228141c66d62286d730b7f
Volodymyr Savchenko <savchenk@ccage007.in2p3.fr>
2016-09-19 17:18:41 +0200

    bug

--- setup.py
+++ setup.py
@@ -7,5 +7,3 @@ setup(
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
         )
-~                                                                                                                                                                                         
-~                          
