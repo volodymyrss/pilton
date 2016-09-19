efedc8cc6ca95e694a0dcff0cb0e2bf319536c17
Volodymyr Savchenko <savchenk@ccage007.in2p3.fr>
2016-09-19 17:18:15 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='pilton',
+        version='1.0',
+        packages=['pilton',],
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        long_description=open('README.md').read(),
+        )
+~                                                                                                                                                                                         
+~                          
