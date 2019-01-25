16bf0fb76fe790deb5a203a80f3faa9822836064
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2019-01-25 19:44:03 +0100

    more test

--- unit_tests/test_pilton.py
+++ unit_tests/test_pilton.py
@@ -19,11 +19,20 @@ class TestStringMethods(unittest.TestCase):
 
     def test_run(self):
         import pilton
+        import os
+
+        CFITSIO_INCLUDE_FILES = os.environ['CFITSIO_INCLUDE_FILES']
+        print("CFITSIO_INCLUDE_FILES before",CFITSIO_INCLUDE_FILES)
+
         pt=pilton.heatool("converttime")
         pt['informat']="IJD"
         pt['intime']="3000"
         pt['outformat'] = "UTC"
         pt.run()
+        
+        CFITSIO_INCLUDE_FILES = os.environ['CFITSIO_INCLUDE_FILES']
+        print("CFITSIO_INCLUDE_FILES after",CFITSIO_INCLUDE_FILES)
+
 
         self.assertFalse(re.search("2008-03-18T23:58:54.815",pt.output) is None)
 
