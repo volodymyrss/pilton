e4849e8b92fce3049ccf10a1625b30993e9b4933
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-04 20:29:18 +0100

    embed colors

--- setup.py
+++ setup.py
@@ -14,4 +14,7 @@ setup(
             },
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
         long_description=open('README.md').read(),
+        install_requires=[
+            'python-shell-colors'
+        ]
         )
