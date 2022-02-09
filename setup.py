e1ac14d6a2dce7ec96f7ca72fc80d376941eacba
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-02-09 09:05:47 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='pilton',
-        version='1.1.0',
+        version='1.1.1',
         py_modules= ['pilton'],
         package_data     = {
             "": [
